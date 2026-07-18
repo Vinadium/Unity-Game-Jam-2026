d6e8ad25aa46698081c21d76f6ebb4e8aca5e2bb
DankKid <jmanholt929@gmail.com>
2026-07-17 20:35:55 -0400

    Peg Stuff

--- Assets/Scripts/IdleGameHandler.cs
+++ Assets/Scripts/IdleGameHandler.cs
@@ -6,6 +6,28 @@ public class IdleGameHandler : MonoBehaviour
     public float pegMultUpgradeCost;
     public float scaleAmount;
 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
 
-    
+            if (hit.collider != null && hit.collider.CompareTag("Peg"))
+            {
+                Debug.Log("Clicked Peg");
+            }
+        }
+    }
+
+    public void AddScales(float value)
+    {
+        scaleAmount += value;
+    }
+
+    public void RemoveScales(float value)
+    {
+        scaleAmount -= value;
+    }
+
 }
